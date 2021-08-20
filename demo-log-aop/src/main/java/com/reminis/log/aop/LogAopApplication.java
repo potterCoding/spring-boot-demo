1cb4a83fa3902b72f05c5967c25f19867a2abe39
reminis <2949079311@qq.com>
2021-08-20 15:03:52 +0800

    此 demo 主要是演示如何使用 aop 切面对请求进行日志记录，并且记录 UserAgent 信息。

---
+++ demo-log-aop/src/main/java/com/reminis/log/aop/LogAopApplication.java
@@ -0,0 +1,17 @@
+package com.reminis.log.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sql
+ * @version 1.0.0
+ * @date 2021/8/20 14:54
+ */
+@SpringBootApplication
+public class LogAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogAopApplication.class, args);
+    }
+}
